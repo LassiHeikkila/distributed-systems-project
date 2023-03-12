8fe3a6180035e00a20a3c6a642e721b94fe3d9c5
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2023-03-12 20:48:02 +0200

    working on PoC frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
@@ -6,38 +6,56 @@ import './App.css'
 
 function App() {
   const [username, setUsername] = useState("");
-  const [roomID, setRoomID] = useState("ab12");
-  const [contentID, setContentID] = useState("ab2b6c94-47ff-41e5-b36f-f66266af8752");
+  const [roomID, setRoomID] = useState("");
+  const [contentID, setContentID] = useState("");
+  const [availableContent, setAvailableContent] = useState([]);
   const [roomDetails, setRoomDetails] = useState(null);
+  const [peerServer, setPeerServer] = useState("");
+
+  const [timer, setTimer] = useState(0);
 
   const roomServiceAddr = 'http://localhost:8081';
   const videoDownloadAddr = 'http://localhost:8080/video/download';
+  const videoSearchAddr = 'http://localhost:8080/video/search'
 
   const fetchRoomDetails = () => {
     fetch(`${roomServiceAddr}/room/details/${roomID}`, {
       method: 'GET',
       mode: 'cors',
-    }).then(resp => {
-      if (resp.status == 200) {
-        setRoomDetails(resp.json());
-      } else {
-        console.error('non-200 response to joining room:', resp.status);
-      }
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      setRoomDetails(data);
+    })
+    .catch(err => {
+      console.error("error fetching room details:", err)
     })
   };
 
-
+  const fetchAvailableContent = () => {
+    fetch(`${videoSearchAddr}`)
+      .then(resp => resp.json())
+      .then(data => {
+        setAvailableContent(data);
+      })
+      .catch(err => {
+        console.error('error searching content:', err)
+      })
+  }
 
   const leaveRoom = () => {
     fetch(`${roomServiceAddr}/room/leave/${username}`, {
       method: 'POST',
       mode: 'cors',
-    }).then(resp => {
-      if (resp.status == 200) {
+    })
+    .then(resp => resp.json())
+    .then(data => {
         setRoomDetails(null);
-      } else {
-        console.error('non-200 response to leaving room:', resp.status);
-      }
+        setRoomID("");
+        clearInterval(timer);
+    })
+    .catch(err => {
+        console.error('error leaving room:', err);
     })
   };
 
@@ -49,16 +67,36 @@ function App() {
       mode: 'cors',
     }).then(resp => {
       if (resp.status == 200) {
-        console.info('joined room!')
-        fetchRoomDetails()
+        console.info('joined room!');
+        fetchRoomDetails();
+        let timerId = setInterval(fetchRoomDetails, 5000);
+        setTimer(timerId);
       }
     })
   };
 
+  useEffect(() => {
+    if (availableContent.length > 0) {
+      setContentID(availableContent[0]['contentID']);
+    }
+  }, [availableContent]);
+
+  useEffect(() => {
+    if (roomDetails == null) {
+      setPeerServer("");
+    } else {
+      setPeerServer(roomDetails.peerServerAddr);
+    }
+  }, [roomDetails]);
+
+  useEffect(() => {
+    fetchAvailableContent();
+  }, []);
+
   return (
     <div className="App">
       <h1>FLMnCHLL</h1>
-      <h2>Due to time constraints, this is a PoC.</h2>
+      <h2>Due to time constraints, this is a very simple PoC.</h2>
       <h2>There is a pre-existing room with short-id: ab12</h2>
       { roomDetails == null ? 
         (
@@ -66,16 +104,14 @@ function App() {
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
               <Form.Label>Enter room id</Form.Label>
               <Form.Control 
-                as="textarea" 
-                rows={1} 
+                type="text"
                 placeholder='id'
                 value={roomID}
                 onChange={(e) => setRoomID(e.target.value) }
               />
               <Form.Label>Enter username</Form.Label>
               <Form.Control 
-                as="textarea" 
-                rows={1} 
+                type="text"
                 placeholder='username'
                 value={username}
                 onChange={(e) => setUsername(e.target.value) }
@@ -104,6 +140,11 @@ function App() {
               </p>
             </video>
           </div>
+          <p>Users present: 
+          { roomDetails.users.map((user) => (
+            <> {user.name} </>
+          )) }
+          </p>
           </>
         )
       }
